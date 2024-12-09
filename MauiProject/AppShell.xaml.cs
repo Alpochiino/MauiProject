7b7f94292025d2cd11d42d97882c15c3bd8822a7
Christian Alp <Chrillealp11@hotmail.com>
2024-12-09 23:10:05 +0100

    Folder changes and small fixes, nothing big

--- MauiProject/AppShell.xaml.cs
+++ MauiProject/AppShell.xaml.cs
@@ -1,10 +1,10 @@
-﻿namespace MauiProject
+﻿namespace MauiProject;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
+
